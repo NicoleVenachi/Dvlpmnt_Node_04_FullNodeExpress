fc7506e468cb72237b0230a2859bfa69c813ea4a
nicolevenachi <ludwing@unicauca.edu.co>
2023-10-23 14:54:52 -0500

    npm and json revier -02- json format testing

--- 02_npmAndJSON/01_json-demo.js
+++ 02_npmAndJSON/01_json-demo.js
@@ -1,4 +1,16 @@
 
 const testJSON = require('./01_test.json')//traigamos el JSON así en local
 
-console.log(testJSON);
+//console.log(testJSON);
+console.log(typeof testJSON); //al importarlo es JSON
+
+//cuirosemos pues el objeto
+console.log(testJSON.temas);
+console.log(testJSON.esPublico);
+
+// convierto objeto en JSON (texto)
+console.log(JSON.stringify(testJSON));
+console.log(typeof JSON.stringify(testJSON));
+
+// convierto JSON en objeto
+console.log(JSON.parse(JSON.stringify(testJSON)));
