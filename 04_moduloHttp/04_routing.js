8b74b7a0d5e0200cb39988531bf6cc9e11778127
nicolevenachi <ludwing@unicauca.edu.co>
2023-10-28 10:18:34 -0500

    http module -05- routing using node -> GET and POST requests' created

--- 04_moduloHttp/04_routing.js
+++ 04_moduloHttp/04_routing.js
@@ -14,7 +14,10 @@ const server = http.createServer((req,res) => {
     case 'GET':
       
       return handleGetRequest(req,res)
-      break;
+    
+    case 'POST':
+
+      return handlePostRequest(req,res)
 
     default:
 
@@ -23,7 +26,7 @@ const server = http.createServer((req,res) => {
   }
 })
 
-// defino como manejo la request
+// defino como manejo la request GET
 function handleGetRequest(req,res) {
   const {url: path} = req //saco path de la request
 
@@ -31,14 +34,33 @@ function handleGetRequest(req,res) {
   if (path === '/') {
     // en rooot, digo que tod bien y mando respuesta
     res.statusCode = 200;
-    res.end('Welco to this server and API created using Node')
+    return res.end('Welco to this server and API created using Node')
   } else if (path === '/courses') {
       // en /cursos, mando la info de todos los cursos
       res.statusCode = 200;
-      res.end(JSON.stringify(courses.coursesInfo)) //mando en respuesta los datos de los cursos (en forma ee texto apra Tx)
-  } else {
-    
+      return res.end(JSON.stringify(courses.coursesInfo)) //mando en respuesta los datos de los cursos (en forma ee texto apra Tx)
+  } else if(path === '/courses/programming') {
+     // en /cursos/programming, mando la info de los cursos de programacion
+      res.statusCode = 200;
+      return res.end(JSON.stringify(courses.coursesInfo.programming)) //mando en respuesta los datos de los cursos de programacion
   }
+
+    // en cualquier otra ruta, error y digo que recurso no existe
+    res.statusCode = 400;
+    return res.end("The requested resource doesn't exists") 
+}
+
+// defino como manejo la request POST
+function handlePostRequest(req,res) {
+  const {url: path} = req //saco path de la request
+
+  //valido request por path
+  if (path === '/courses/programming') {
+    // vamos a agregar cursos en programacion (simulado de momento)
+    res.statusCode = 200;
+    return res.end('The server reeibed a POST request on /courses/programming')
+  }
+
 }
 
 
