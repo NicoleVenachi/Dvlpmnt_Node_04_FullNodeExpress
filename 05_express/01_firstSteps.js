467e0c8ca77af4d9b3710de9636b46629f69e66c
nicolevenachi <ludwing@unicauca.edu.co>
2023-10-28 16:51:36 -0500

    express - 02- routes definition

--- 05_express/01_firstSteps.js
+++ 05_express/01_firstSteps.js
@@ -9,10 +9,24 @@ const {coursesInfo} = require('./cursos') //importo la data hardcodeada
 // console.log(coursesInfo);
 
 // ***Routing ***
-app.get('/', (req,res) => {
-  res.send('All courses')
+app.get('/', (req,res) => { //root (home)
+  res.send('Welcome to the server')
 })
 
+app.get('/api/courses', (req,res) => { //all courses
+  res.send(JSON.stringify(coursesInfo)) //envio pero antes de Tx lo paso a texto
+})
+
+app.get('/api/courses/programming', (req,res) => { //programming courses
+  res.send(JSON.stringify(coursesInfo.programming)) //envio pero antes de Tx lo paso a texto
+})
+
+app.get('/api/courses/mathematics', (req,res) => { //mathematics courses
+  res.send(JSON.stringify(coursesInfo.mathematics)) //envio pero antes de Tx lo paso a texto
+})
+
+
+// port definition
 const PORT = process.env.PORT || 3000; //regularmente, el servicio de deploy me asigna el puerto
 
 app.listen(PORT, () => {
