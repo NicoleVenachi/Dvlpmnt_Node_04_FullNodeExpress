b80844ca4cba2a7ae6606fc57ef01698cc85f95f
nicolevenachi <ludwing@unicauca.edu.co>
2023-10-29 05:53:45 -0500

    express -06- routers review

--- 05_express/01_firstSteps.js
+++ 05_express/01_firstSteps.js
@@ -8,6 +8,14 @@ const {coursesInfo} = require('./cursos') //importo la data hardcodeada
 
 // console.log(coursesInfo);
 
+//  *********** Routers *******
+
+const routerProgramming = express.Router() //creo router
+app.use('/api/courses/programming', routerProgramming) //asigno router a un path
+
+const routerMathematics = express.Router();
+app.use('/api/courses/mathematics', routerMathematics)
+
 // ***Routing ***
 app.get('/', (req,res) => { //root (home)
   res.send('Welcome to the server')
@@ -17,11 +25,11 @@ app.get('/api/courses', (req,res) => { //all courses
   res.send(JSON.stringify(coursesInfo)) //envio pero antes de Tx lo paso a texto
 })
 
-app.get('/api/courses/programming', (req,res) => { //programming courses
+routerProgramming.get('/', (req,res) => { //programming courses
   res.send(JSON.stringify(coursesInfo.programming)) //envio pero antes de Tx lo paso a texto
 })
 
-app.get('/api/courses/programming/:language', (req,res) => { //handle :URL PARAMETERS
+routerProgramming.get('/:language', (req,res) => { //handle :URL PARAMETERS
 
   // console.log(req.params);
   const language = req.params.language //saco los url params
@@ -44,7 +52,7 @@ app.get('/api/courses/programming/:language', (req,res) => { //handle :URL PARAM
 })
 
 
-app.get('/api/courses/programming/:language/:level', (req,res) => { //handle :URL PARAMETERS (filtrar por 2 URL params)
+routerProgramming.get('/api/courses/programming/:language/:level', (req,res) => { //handle :URL PARAMETERS (filtrar por 2 URL params)
 
   // console.log(req.params);
   const {language, level} = req.params //saco los url params
@@ -65,11 +73,11 @@ app.get('/api/courses/programming/:language/:level', (req,res) => { //handle :UR
 
 
 
-app.get('/api/courses/mathematics', (req,res) => { //mathematics courses
+routerMathematics.get('/', (req,res) => { //mathematics courses
   res.send(JSON.stringify(coursesInfo.mathematics)) //envio pero antes de Tx lo paso a texto
 })
 
-app.get('/api/courses/mathematics/:topic', (req,res) => { //handle :URL PARAMETERS
+routerMathematics.get('/:topic', (req,res) => { //handle :URL PARAMETERS
 
   const topic = req.params.topic;
 
